977d3d29ba62397fb704c67b312d62086d86a51a
s-yipt <s-yipt@53S50030513.it.bsd405.org>
2019-11-22 14:53:42 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -11,9 +11,14 @@ public class FracCalc {
     public static void main(String[] args) 
     { 
         // TODO: Read the input from the user and call produceAnswer with an equation
-    	Scanner console = new Scanner(System.in);
-    	String input = console.nextLine();
-    	System.out.println(produceAnswer(input));
+    	Scanner s = new Scanner(System.in);
+    	System.out.print("Enter your fraction problem: "); 
+    	String input = s.nextLine();
+    	while(!input.equalsIgnoreCase("quit")) {
+    		System.out.println(produceAnswer(input));
+    		System.out.println("Enter your fraction problem: ");
+    		input = s.nextLine();
+    	}
 
     }
 
@@ -25,18 +30,52 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input){
         // TODO: Implement this function to produce the solution to the input
     	String firstOp =
-    			input.substring(0,input.indexOf(' '));
+    	input.substring(0,input.indexOf(' '));
     	String secondOp = 
-    			input.substring(input.indexOf(' ') + 3, input.length());
+    	input.substring(input.indexOf(' ') + 3, input.length());
     	String operator = input.substring(input.indexOf(' ') + 1,input.indexOf(' ') + 2);
-    	return secondOp;
+    	
+    	
+    	String op2Whole = findWhole(secondOp); 
+    	String op2Num = findNum(secondOp); 
+    	String op2Deno = findDeno(secondOp); 
+    	
+    	String theAnswer = "whole:" + op2Whole + " numerator:" + op2Num + " denominator:" + op2Deno;
+    	
+    	return theAnswer; 
     	
   }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
-
+  // TODO: Fill in the space below with any helper methods that you think you will need
+    public static String findWhole(String str) {
+    	if (str.contains("_")) {
+    		return str.substring(0, str.indexOf('_'));
+    	}
+    	if (str.contains("/")) {
+    		return "0"; 
+    	}
+    	else return str;
+    }
+    public static String findNum(String str) {
+    	if (str.contains("_")) {
+    		return str.substring(str.indexOf('_') + 1, str.indexOf('/'));
+    	}
+    	else if(str.contains("/")) {
+    		return str.substring(0, str.indexOf('/'));
+    	}
+    	else {
+    		return "0";  
+    	}
+    }
+    public static String findDeno(String str) {
+    	if(str.contains("/")) {
+    		return str.substring(str.indexOf("/") + 1);
+    	}
+    	else {
+    		return "1"; 
+    	}
+    }
 }
